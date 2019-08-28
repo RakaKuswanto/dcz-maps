f28f0d16ac6d891760d582dca2e2b754e63b1e27
RakaKuswanto <raka.kuswanto@gmail.com>
2019-08-28 16:45:23 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,17 @@
 import os
 from src.app import create_app
 
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
+
 if __name__ == '__main__':
   app = os.environ['FLASK_ENV']
   # run app
