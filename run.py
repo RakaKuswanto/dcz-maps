11d0efed974d96fcff1b0e9eb10dbe2638649e30
RakaKuswanto <raka.kuswanto@gmail.com>
2019-08-28 18:51:13 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,12 @@
 import os
 from src.app import create_app
 
-def mulai(environ, start_response):    
+
+env_name  =   os.environ['FLASK_ENV']
+app = create_app(env_name)
+
+def mulai(environ, start_response):
+    app.run()
     # run app
     """Simplest possible application object"""
     data = b'Hello, World!\n'
@@ -14,7 +19,7 @@ def mulai(environ, start_response):
     start_response(status, response_headers)
     return iter([data])
 
-if __name__ == '__main__':
-    env_name  =   os.environ['FLASK_ENV']
-    app = create_app(env_name)
-    app.run()
+# if __name__ == '__main__':
+#     env_name  =   os.environ['FLASK_ENV']
+#     app = create_app(env_name)
+#     app.run()
